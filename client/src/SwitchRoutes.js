726598ecdfa8ffe1b5b5850f936b58511b46ace2
SBrendan <brendan.seiller2016@campus-eni.fr>
2020-02-13 19:32:35 +0100

    fix heroku

--- client/src/SwitchRoutes.js
+++ client/src/SwitchRoutes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import routes from "./routes.js";
+import routes from "./routes.js.js";
 
 const switchRoutes = (
     <Switch>
