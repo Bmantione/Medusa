b308ce8793a81042787ed807e1465c785b5da6c1
bseiller <brendan.seiller@students.campus.academy>
2020-02-14 09:09:23 +0100

    add path dep

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
